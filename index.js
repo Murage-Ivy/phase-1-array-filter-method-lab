c6fcbaf6ee61b661a4fd2872c33ea1517b8c0a4c
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-08-18 21:01:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 // Code your solution here
-const drivers = [
-    {
-      name: 'Bobby',
-      hometown: 'Pittsburgh' },
-    {
-      name: 'Sammy',
-      hometown: 'New York' } ,
-    {
-      name: 'Sally',
-      hometown: 'Cleveland' },
-    {
-      name: 'Annette',
-      hometown: 'Los Angeles' },
-    {
-      name: 'Bobby',
-      hometown: 'Tampa Bay' }
-  ];
-
 
 function findMatching(drivers, name) {
   return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
@@ -28,7 +10,5 @@ function fuzzyMatch(drivers, string) {
 }
 
 function matchName(drivers, property) {
-    return drivers.filter(driver => driver.name.toLowerCase() === property.toLowerCase())
+  return drivers.filter(driver => driver.name.toLowerCase() === property.toLowerCase())
 }
-
-console.log(matchName(drivers, 'Bobby'));
